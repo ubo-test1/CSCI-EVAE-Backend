004bd7daa24cca65c56bd133328515ee3d4afacd
Administrator <admin@example.com>
2024-02-12 14:26:48 +0100

    fix 1

---
+++ src/main/java/com/back/csaback/Controllers/EvaluationController.java
@@ -0,0 +1,12 @@
+package com.back.csaback.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("eva")
+public class EvaluationController {
+
+
+
+}
