672994243bed0b31a60e86d7b77f08eaed775cd0
Saad Hadiche <61362146+H-Saad@users.noreply.github.com>
2024-02-16 16:02:02 +0100

    Update EvaluationRepository.java

--- src/main/java/com/back/csaback/Repositories/EvaluationRepository.java
+++ src/main/java/com/back/csaback/Repositories/EvaluationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EvaluationRepository extends JpaRepository<Evaluation,Long> {
+public interface EvaluationRepository extends JpaRepository<Evaluation,Integer> {
 }
