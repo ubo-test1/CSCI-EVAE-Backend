515cb8616553cef51a563f97ddf08d4ab8a30212
Administrator <admin@example.com>
2024-02-21 15:44:31 +0100

    bugfixes

--- src/main/java/com/back/csaback/Controllers/EvaluationController.java
+++ src/main/java/com/back/csaback/Controllers/EvaluationController.java
@@ -7,10 +7,7 @@ import com.back.csaback.Services.EvaluationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
